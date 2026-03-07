65d027b3036fa27e4dd160f48c7f16151eaddb59
Your Name <you@example.com>
2026-03-07 22:50:34 +0530

    created .env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBh_BiBBLgmP39QPoCNomXsHwd50HJ-3Bw",
-  authDomain: "netflix-gpt-a4e35.firebaseapp.com",
-  projectId: "netflix-gpt-a4e35",
-  storageBucket: "netflix-gpt-a4e35.appspot.com",
-  messagingSenderId: "26738907692",
-  appId: "1:26738907692:web:4ecdef0c09a6e4c1608f89"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
